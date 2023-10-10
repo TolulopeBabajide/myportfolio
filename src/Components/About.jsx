c28e76b6a74ec54b294c0af3318cd113c68d4ad3
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-10-10 11:12:20 +0100

    about component created

---
+++ src/Components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex h-full justify-between items-center'>
+        <div className='w-1/2'>
+            Picture
+        </div>
+        <div className='w-1/2 items-center text-justify'>
+            <h1 className='text-5xl'>Tolulope Babajide</h1>
+            <h2 className='text-2xl'>Fullstack Software Developer</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, non! Magni minima tenetur impedit. Nemo?</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/Components/Mainpage.jsx
+++ src/Components/Mainpage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Nav from './Nav'
+import About from './About'
 
 const Mainpage = () => {
   return (
     <div className='h-screen'>
         <Nav/>
-        <div className='h-1/2 flex justify-between p-2 bg-blue-500'>
-          <div>picture</div>
-          <div>about</div>
+        <div className='h-1/2 p-2 bg-blue-500'>
+          <About/>
         </div>
         <div className='h-1/2 p-2 bg-slate-200'>
           projects
