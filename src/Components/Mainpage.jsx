789ce2886a40c426b231e07104a551d9cdf1f224
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-10-10 09:19:21 +0100

    mainpage template created

--- src/Components/Mainpage.jsx
+++ src/Components/Mainpage.jsx
@@ -3,8 +3,15 @@ import Nav from './Nav'
 
 const Mainpage = () => {
   return (
-    <div>
+    <div className='h-screen'>
         <Nav/>
+        <div className='h-1/2 flex justify-between p-2 bg-blue-500'>
+          <div>picture</div>
+          <div>about</div>
+        </div>
+        <div className='h-1/2 p-2 bg-slate-200'>
+          projects
+        </div>
     </div>
   )
 }
